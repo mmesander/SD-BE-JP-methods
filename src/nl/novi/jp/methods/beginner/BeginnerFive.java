745af17b3d4fb4017f9b02917e9a7903830577c8
Mark Mesander <mark_mesander@hotmail.com>
2023-09-07 16:58:48 +0200

    add BeginnerFive

--- src/nl/novi/jp/methods/beginner/BeginnerFive.java
+++ src/nl/novi/jp/methods/beginner/BeginnerFive.java
@@ -2,8 +2,15 @@ package nl.novi.jp.methods.beginner;
 
 /**
  * Hieronder is een lege klasse. Maak hierin een methode die start() heet. Deze methode print "Power on" uit.
- *
+
  * Zorg ervoor dat de methode wordt aangeroepen en dat de tekst uitgeprint wordt.
  */
 public class BeginnerFive {
+    public static void main(String[] args) {
+        start();
+    }
+
+    private static void start() {
+        System.out.println("Power on");
+    }
 }
